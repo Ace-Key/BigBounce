eb2a23cd7c243838f0c2025ba32c15797f40818b
Pita T.H <key.lockcard22@gmail.com>
2021-02-11 15:36:47 +1300

    initial commit
    first commit to text-based game project

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] State startingState;
+    [SerializeField] Text textComponent;
+    
+    State currentState;
+   // calls the starting options
+    void Start()
+    {
+        currentState = startingState;
+        textComponent.text = currentState.GetStoryState();
+        
+    }
+    //calls the methods that manage the game state
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = currentState.GetNextState();
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentState = nextStates[0]; 
+        }
+        else if (Input.GetKeyDown(KeyCode.B))
+        {
+            currentState = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            currentState = nextStates[2];
+        }
+        textComponent.text = currentState.GetStoryState();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+
+  [TextArea(10,14)][SerializeField] string storyText;
+  [SerializeField] State[] nextStates;
+    public string GetStoryState() 
+    {
+        return storyText;
+    }
+
+    public State[] GetNextState()
+    {
+        return nextStates;
+    }
+}
